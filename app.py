0781b4066e9dfd4fe1389ae5672020a3d400993f
EzequielIrala <ezequiellirala@gmail.com>
2023-02-18 01:40:12 -0500

    hice cambios en los directiorios por necesidad del flask, chusmea, mañana sigo con lo de file, te dejé para que hagas algo de cards en Productos, te arreglé lo de 'todos' (productos), agregué sombras doradas, etc. Chusmea

---
+++ app.py
@@ -0,0 +1,45 @@
+from io import BytesIO
+from flask import Flask, render_template, redirect, json, jsonify, request
+import traceback
+from base import Productos, session, insert
+
+app = Flask(__name__, static_folder='staticFiles', template_folder='templates')
+
+@app.route("/")
+def index():
+    try:
+        return render_template('index.html')
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+
+@app.route("/productos", methods=['GET'])
+def productos():
+    if request.method == 'GET':
+        try:
+            return render_template('productos.html')
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+
+@app.route("/abm", methods=['GET'])
+def abm():
+    if request.method == 'GET':
+        return render_template('abm.html')
+    #elif request.method == 'POST':
+    #    file = request.files['file']
+    #    upload = Upload(name=file.filename, data=file.read())
+    #    session.add(upload)
+    #    session.commit()
+    #    return f'Uploaded: {file.filename}
+
+@app.route("/nuevo", methods=['POST'])
+def nuevo():
+    if request.method == 'POST':
+        n = str(request.form.get('name'))#name
+        d = str(request.form.get('description'))#description
+        p = request.form.get('price')#price
+        insert(n, d, p)
+
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host="172.24.64.1", port=5000)
---
+++ base.py
@@ -0,0 +1,47 @@
+import sqlalchemy
+
+from sqlalchemy import Column, Integer, String, LargeBinary
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+from flask import jsonify
+
+engine = sqlalchemy.create_engine('sqlite:///database.db')
+
+base = declarative_base(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Productos(base):
+    __tablename__ = "Productos"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    description = Column(String(50))
+    #data = Column(LargeBinary)
+    price = Column(Integer)
+
+    def __repr__(self):
+        return self
+    def __repr__(self):
+        return ({
+            'id' : self.id,
+            'name' : self.name,
+            'description' : self.description,
+            #'data' : self.data,
+            'price' : self.price
+        })
+
+def insert(name, description, price):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    a = Productos(name=name, description=description, price=price)
+    
+    session.add(a)
+    session.commit()
+
+if __name__ == "__main__":
+    base.metadata.create_all(engine)
